84de4aa8a37db74610e96b35c15ba7134c8a9322
Viamez <jose_viam@hotmail.com>
2022-10-14 22:50:42 -0500

    Cambiamos la logica

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,73 +1,72 @@
-import React, {
-  useState,
-  useReducer,
-  useMemo,
-  useRef,
-  useCallback,
-} from "react";
+import React, { useState, useMemo } from "react";
 import { useCharacters } from "../hooks/useCharacters";
 import "../styles/Characters.css";
 import "../styles/Characters2.css";
-import { Search } from "./Search";
+import "../styles/Search.css";
 const APICharacter = "https://rickandmortyapi.com/api/character";
 
 const Characters = () => {
-  const [search, setSearch] = useState("");
-  const searchInput = useRef(null);
-
   const characters = useCharacters(APICharacter);
+  const [search, setSearch] = useState("");
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
 
-  const handleSearch = useCallback(() => {
-    setSearch(searchInput.current.value);
-  }, []);
+  const filteredUsers = characters.filter((user) => {
+    const userName=user.name.toLowerCase().includes(search.toLowerCase()) 
+    const userLocation= user.location.name.toLowerCase().includes(search.toLowerCase())
+    return (userName|| userLocation)
+  });
 
-  const filteredUsers = useMemo(
-    () =>
-      characters.filter((user) => {
-        return (
-          user.name.toLowerCase().includes(search.toLowerCase()) ||
-          user.location.name.toLowerCase().includes(search.toLowerCase())
-        );
-      }),
-    [characters, search]
-  );
+
+  
 
   return (
     <section className="main-container">
-      <Search
-        search={search}
-        searchInput={searchInput}
-        handleSearch={handleSearch}
-      />
-      {!!search ? (
-        <div className="character-container">
-          {filteredUsers.map((character) => (
-            <div className="character-card" key={character.id}>
-              <img src={character.image} key={character.id} />
-              <div className="character-info">
-                <div>
-                  <p key={character.name}>{character.name} </p>
-                  <p key={character.gender}>{character.gender}</p>
-                </div>
-                <p className="character-location" key={character.location.name}>
-                  {character.location.name}
-                </p>
-              </div>
+      <div>
+        <input
+          className="Search-character"
+          placeholder="Busca tu personaje"
+          type="text"
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+      {!search?
+      <div className="character-container">
+      {filteredUsers.map((character) => (
+        <div className="character-card" key={character.id}>
+          <img src={character.image} key={character.id} />
+          <div className="character-info">
+            <div>
+              <p key={character.name}>{character.name} </p>
+              {/* <p key={character.gender}>{character.gender}</p> */}
             </div>
-          ))}
+            {/* <p className="character-location" key={character.location.name}>
+              {character.location.name}
+            </p> */}
+          </div>
         </div>
-      ) : (
-        <div className="character-container">
-          {filteredUsers.map((character) => (
-            <div className="character-card" key={character.id}>
-              <div className="character-info">
-                <p key={character.name}>{character.name}</p>
-              </div>
-              <img src={character.image} key={character.id} />
-            </div>
-          ))}
+      ))}
+    </div>:
+    <div className="character-container">
+    {filteredUsers.map((character) => (
+      <div className="character-card" key={character.id}>
+        <img src={character.image} key={character.id} />
+        <div className="character-info">
+          {/* <div>
+            <p key={character.name}>{character.name} </p>
+            <p key={character.gender}>{character.gender}</p>
+          </div> */}
+          <p className="character-location" key={character.location.name}>
+            {character.location.name}
+          </p>
         </div>
-      )}
+      </div>
+    ))}
+  </div>
+    }
+      
     </section>
   );
 };
