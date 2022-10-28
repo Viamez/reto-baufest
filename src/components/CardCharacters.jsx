8d6f8737ff39244036fe03690ad663a861931159
Viamez <jose_viam@hotmail.com>
2022-10-28 00:06:08 -0500

    Hicimos las comparaciones

--- src/components/CardCharacters.jsx
+++ src/components/CardCharacters.jsx
@@ -1,18 +1,19 @@
 import React, { useReducer, useState } from "react";
 import { useApi } from "../hooks/useApi";
 import { useCharacter } from "../hooks/useCharacter";
+import { Modal } from "../Modal";
 import "../styles/Characters.css";
 import { Search } from "./Search";
 
 const initialState = {
-  comparativeList: [],
+  comparateList: [],
 };
-const comparativeReducer = (state, action) => {
+const comparateReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_COMPARATIVE":
       return {
         ...state,
-        comparativeList: [...state.comparativeList, action.payload],
+        comparateList: [...state.comparateList, action.payload],
       };
     default:
       return state;
@@ -20,7 +21,7 @@ const comparativeReducer = (state, action) => {
 };
 
 const CardCharacters = () => {
-  const [comparar, dispatch] = useReducer(comparativeReducer, initialState);
+  const [comparar, dispatch] = useReducer(comparateReducer, initialState);
   const APICharacter = "https://rickandmortyapi.com/api/character";
   const APIEpisodes = "https://rickandmortyapi.com/api/episode";
   const characters = useApi(APICharacter);
@@ -40,31 +41,56 @@ const CardCharacters = () => {
   };
 
   const [search, handleSearch, filteredDato] = useCharacter(characters);
-  
-  let episodiosCompartidos = [];
-  episodiosCompartidos = comparar.comparativeList.map((element) => {
+
+  let episodesMap = comparar.comparateList.map((element) => {
     const episodes = element.episodesName;
-    return episodes;
+    const name = element.name;
+    return { episodes, name };
   });
-  function compararCharacters(datos){
-    if(1< datos.length <3){
-      const  firstSet= new Set(datos[0])
-      const  secondSet= new Set(datos[1])
-      const commonElements = [...firstSet].filter(element => secondSet.has(element));
-      console.log(commonElements)
-      console.log(datos.length)
-    }else{
-      console.log('no puedes comparar mas')
+  function compararCharacters(datos) {
+    if (datos.length == 2) {
+      const firstSet = new Set(datos[0].episodes);
+      const secondSet = new Set(datos[1].episodes);
+      const firstSecond = [...firstSet].filter((element) =>
+        secondSet.has(element)
+      );
+      console.log(`${datos[0].name} y ${datos[1].name}`, firstSecond);
+    } else if (datos.length == 3) {
+      const firstSet = new Set(datos[0].episodes);
+      const secondSet = new Set(datos[1].episodes);
+      const thirdSet = new Set(datos[2].episodes);
+      const firstSecond = [...firstSet].filter((element) =>
+        secondSet.has(element)
+      );
+      const firstThird = [...firstSet].filter((element) =>
+        thirdSet.has(element)
+      );
+      const secondThird = [...secondSet].filter((element) =>
+        thirdSet.has(element)
+      );
+      console.log(`${datos[0].name} y ${datos[1].name}`, firstSecond);
+      console.log(`${datos[0].name} y ${datos[2].name}`, firstThird);
+      console.log(`${datos[1].name} y ${datos[2].name}`, secondThird);
+    } else {
+      console.log("Solo puedes comparar un maximo de 3 personajes");
     }
   }
-  
-
 
   return (
     <section className="main-container">
+      <Modal>Aqui renderizamos las comparaciones</Modal>
+      <div className="search-comparate">
+        <Search search={search} handleSearch={handleSearch} />
+        <button
+          type="button"
+          className="button-Comparate"
+          onClick={() => compararCharacters(episodesMap)}
+        >
+          Comparar
+        </button>
+      </div>
       <div>
-      <Search search={search} handleSearch={handleSearch} />
-      <button type="button" onClick={()=>compararCharacters(episodiosCompartidos)} >Comparar</button>
+        
       </div>
       <div className="character-container">
         {filteredDato.map((character) => (
@@ -74,7 +100,11 @@ const CardCharacters = () => {
               <div>
                 <p>{character.name} </p>
                 <p key={character.gender}>{character.gender}</p>
-                <button type="button" onClick={() => handleClick(character)}>
+                <button
+                  type="button"
+                  className="button-filter"
+                  onClick={() => handleClick(character)}
+                >
                   Comparar
                 </button>
               </div>
